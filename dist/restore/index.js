d694fe0b3ff75adf7b66c8769f44be8351a30062
Marco Selleri <marco.selleri96@gmail.com>
2024-09-18 00:27:15 +0200

    Switch to proper actions

--- dist/restore/index.js
+++ dist/restore/index.js
@@ -32616,6 +32616,53 @@ var State;
 
 /***/ }),
 
+/***/ 2582:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const core = __importStar(__nccwpck_require__(115));
+const restoreImpl_1 = __importDefault(__nccwpck_require__(4013));
+const stateProvider_1 = __nccwpck_require__(2968);
+async function run() {
+    await (0, restoreImpl_1.default)(new stateProvider_1.StateProvider());
+}
+run()
+    .catch((error) => {
+    core.setFailed(error.message);
+});
+exports["default"] = run;
+
+
+/***/ }),
+
 /***/ 4013:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -32687,53 +32734,6 @@ async function restoreImpl(stateProvider) {
     return cacheKey;
 }
 exports["default"] = restoreImpl;
-
-
-/***/ }),
-
-/***/ 8481:
-/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
-
-"use strict";
-
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core = __importStar(__nccwpck_require__(115));
-const restoreImpl_1 = __importDefault(__nccwpck_require__(4013));
-const stateProvider_1 = __nccwpck_require__(2968);
-async function run() {
-    await (0, restoreImpl_1.default)(new stateProvider_1.NullStateProvider());
-}
-run()
-    .catch((error) => {
-    core.setFailed(error.message);
-});
-exports["default"] = run;
 
 
 /***/ }),
@@ -34799,7 +34799,7 @@ module.exports = parseParams
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module is referenced by other modules so it can't be inlined
-/******/ 	var __webpack_exports__ = __nccwpck_require__(8481);
+/******/ 	var __webpack_exports__ = __nccwpck_require__(2582);
 /******/ 	module.exports = __webpack_exports__;
 /******/ 	
 /******/ })()
--- dist/save/index.js
+++ dist/save/index.js
@@ -32616,6 +32616,53 @@ var State;
 
 /***/ }),
 
+/***/ 9470:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const core = __importStar(__nccwpck_require__(115));
+const saveImpl_1 = __importDefault(__nccwpck_require__(8138));
+const stateProvider_1 = __nccwpck_require__(2968);
+async function run() {
+    await (0, saveImpl_1.default)(new stateProvider_1.StateProvider());
+}
+run()
+    .catch((error) => {
+    core.setFailed(error.message);
+});
+exports["default"] = run;
+
+
+/***/ }),
+
 /***/ 8138:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -32684,56 +32731,6 @@ async function saveImpl(stateProvider) {
     return undefined;
 }
 exports["default"] = saveImpl;
-
-
-/***/ }),
-
-/***/ 3565:
-/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
-
-"use strict";
-
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core = __importStar(__nccwpck_require__(115));
-const saveImpl_1 = __importDefault(__nccwpck_require__(8138));
-const stateProvider_1 = __nccwpck_require__(2968);
-async function run() {
-    const cacheId = await (0, saveImpl_1.default)(new stateProvider_1.NullStateProvider());
-    if (cacheId === -1) {
-        core.warning('Cache save failed.');
-    }
-}
-run()
-    .catch((error) => {
-    core.setFailed(error.message);
-});
-exports["default"] = run;
 
 
 /***/ }),
@@ -34799,7 +34796,7 @@ module.exports = parseParams
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module is referenced by other modules so it can't be inlined
-/******/ 	var __webpack_exports__ = __nccwpck_require__(3565);
+/******/ 	var __webpack_exports__ = __nccwpck_require__(9470);
 /******/ 	module.exports = __webpack_exports__;
 /******/ 	
 /******/ })()
